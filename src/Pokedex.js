9f746e5e30fd8471376e6e411f6c16162d1f1e5d
elena-vost <angara1298@gmail.com>
2021-02-15 20:42:51 +0100

    all cards done

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
 
 class Pokedex extends Component {
     static defaultProps = {
@@ -14,7 +15,14 @@ class Pokedex extends Component {
 		]
     };
     render () {
-
+        return (
+            <div className="Pokedex">
+                <h1>Pokedex!</h1>
+                {this.props.pokemon.map((p)=> (
+                    <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+                ))}
+            </div>
+        )
     }
 }
 
