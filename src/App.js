ecfda5643ea6939ba6cbaae7be5a8006548e8401
elena-vost <angara1298@gmail.com>
2021-02-15 19:33:54 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React, {Component} from 'react';
+import Pokecard from './Pokecard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Pokecard />
     </div>
   );
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 class Pokecard extends Component {
     render() {
-        return <h1>POKECARD!</h1>
+        return <h1>POKECARD!</h1>;
     }
 }
+
+export default Pokecard;
